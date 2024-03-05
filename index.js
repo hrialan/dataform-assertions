5b5c7f0ffeab3c9a8af604458472cb03d8eeb7ef
Hugo Rialan <hugo.rialan@devoteamgcloud.com>
2024-03-05 16:06:56 +0100

    init

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const row_condition_assertions = require("./includes/row_condition_assertions");
+const unique_key_assertions = require("./includes/unique_key_assertions");
 
 
 module.exports = ({
     row_condition_assertions_params = {},
+    unique_key_assertions_params = {}
     
 }) => {
 
-    row_condition_assertions(row_condition_assertions_params)
+    row_condition_assertions(row_condition_assertions_params);
+    unique_key_assertions(unique_key_assertions_params);
 
-    return { row_condition_assertions_list };
 }
