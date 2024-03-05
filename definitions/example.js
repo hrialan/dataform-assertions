fa7eadbb95e4e68bc477cbb4ff628e1ac1c10930
Hugo Rialan <hugo.rialan@devoteamgcloud.com>
2024-03-05 16:21:06 +0100

    add return

--- definitions/example.js
+++ definitions/example.js
@@ -1,12 +1,10 @@
 const dataform_assertions = require("../index");
 
 const assertions = dataform_assertions({
-    "row_condition_assertions_params": {
-        "table1": {
-          "pk_not_null": "id IS NOT NULL",
-          "id_strict_positive": "id > 0"
-        }
+  "row_condition_assertions_params": {
+    "table1": {
+      "pk_not_null": "id IS NOT NULL",
+      "id_strict_positive": "id > 0"
     }
+  }
 });
-
-
--- includes/unique_key_assertions.js
+++ includes/unique_key_assertions.js
@@ -5,29 +5,33 @@
 // };
 
 module.exports = (uniqueKeyConditions) => {
+  const assertions = [];
 
-    // Do not modify this function unless you have specific requirements.
-    const createUniqueKeyAssertion = (tableName, columns) => {
-        const uniqueColumns = columns.join(', ');
+  // Do not modify this function unless you have specific requirements.
+  const createUniqueKeyAssertion = (tableName, columns) => {
+    const uniqueColumns = columns.join(', ');
 
-        assert(`assert_unique_key_${tableName}`)
-            // .database(dataform.projectConfig.vars.project + dataform.projectConfig.vars.env)
-            // .schema(`d_${dataform.projectConfig.vars.use_case}_assertions_eu_${dataform.projectConfig.vars.env}`)
-            .description(`Check that values in columns (${uniqueColumns}) in ${tableName} form a unique key`)
-            .tags("assertions")
-            .query(ctx => `SELECT ${uniqueColumns}
+    const assertion = assert(`assert_unique_key_${tableName}`)
+      // .database(dataform.projectConfig.vars.project + dataform.projectConfig.vars.env)
+      // .schema(`d_${dataform.projectConfig.vars.use_case}_assertions_eu_${dataform.projectConfig.vars.env}`)
+      .description(`Check that values in columns (${uniqueColumns}) in ${tableName} form a unique key`)
+      .tags("assertions")
+      .query(ctx => `SELECT ${uniqueColumns}
                        FROM ${ctx.ref(tableName)}
                        GROUP BY ${uniqueColumns}
                        HAVING COUNT(*) > 1`);
-        
-    };
 
-    // Loop through uniqueKeyConditions to create unique key check assertions.
-    // Do not modify this loop unless you have specific requirements.
-    for (let tableName in uniqueKeyConditions) {
-        const columns = uniqueKeyConditions[tableName];
-        createUniqueKeyAssertion(tableName, columns);
-    }
-    // Do not modify this loop unless you have specific requirements.
+    assertions.push(assertion);
 
+  };
+
+  // Loop through uniqueKeyConditions to create unique key check assertions.
+  // Do not modify this loop unless you have specific requirements.
+  for (let tableName in uniqueKeyConditions) {
+    const columns = uniqueKeyConditions[tableName];
+    createUniqueKeyAssertion(tableName, columns);
+  }
+  // Do not modify this loop unless you have specific requirements.
+
+  return assertions;
 }
